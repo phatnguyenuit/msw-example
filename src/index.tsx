72c07822c6de7d30ce435909790b9eb2f56ef579
Nguyen Phat <phatnt.uit@gmail.com>
2021-09-25 12:02:28 +0700

    docs: add code comments

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,14 @@ function prepareMocks() {
   if (process.env.REACT_APP_USE_MOCK === 'true') {
     return import('./mocks/browser').then(({ mswWorker }) =>
       mswWorker.start({
+        onUnhandledRequest: 'bypass',
+        // workaround serviceWorker & findWorker
+        // to use in github pages
         serviceWorker: {
           url: './mockServiceWorker.js',
         },
         findWorker: (scriptURL, _mockServiceWorkerUrl) =>
           scriptURL.includes('mockServiceWorker'),
-        onUnhandledRequest: 'bypass',
       }),
     );
   }
